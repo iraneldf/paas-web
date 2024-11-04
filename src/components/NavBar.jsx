6c4f4fa96a0bef356d9226ce8ae7ea35ec0043e1
iraneldf <iranel831@gmail.com>
2024-11-03 19:23:15 -0500

    Ajustes en home y NavBar.jsx responsivo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,55 @@
-import Image from "next/image";
+'use client'
+import React, { useState } from 'react';
+import Image from 'next/image'; // Asegúrate de que estás importando correctamente el componente Image
+import { Menu } from 'lucide-react'; // Asegúrate de importar el ícono Menu
 
-export default function NavBar() {
+const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
-        <header className="bg-gray-300 text-black">
-            <div className=" mx-auto px-20 py-2 flex justify-between items-center">
+        <header className="bg-gray-300 text-black relative">
+            <div className="mx-auto px-5 xl:px-20 py-2 flex justify-between items-center">
+                <Image src={'/image/Logo PAAS.svg'} alt={'Logo'} width={100} height={50} />
 
-                <Image src={'/image/Logo PAAS.svg'} alt={'Logo'} width={100} height={50}/>
-                <nav>
+                <nav className={'hidden lg:block'}>
                     <ul className="flex space-x-4">
                         <li><a href="#home" className="hover:underline">¿Quiénes somos?</a></li>
                         <li><a href="#services" className="hover:underline">Proyectos en curso</a></li>
                         <li><a href="#about" className="hover:underline">Proyectos terminados</a></li>
                         <li><a href="#contact" className="hover:underline">Productos y resultados</a></li>
-                        <li><a href="#contact" className="hover:underline">Nuestro equipo</a></li>
+                        <li><a href="#team" className="hover:underline">Nuestro equipo</a></li>
+                        <li><a href="#contact" className="hover:underline">Contáctanos</a></li>
+                    </ul>
+                </nav>
+
+                <button className={'lg:hidden'} onClick={toggleMenu}>
+                    <Menu />
+                </button>
+            </div>
+
+            {/* Menú desplegable */}
+            <div
+                className={`lg:hidden bg-gray-300  absolute w-full top-20 z-30 transition-all duration-300 ease-in-out overflow-hidden ${
+                    isMenuOpen ? 'max-h-48' : 'max-h-0'
+                }`}
+            >
+                <nav className="w-full">
+                    <ul className="flex flex-col items-center gap-2 pb-10">
+                        <li><a href="#home" className="hover:underline">¿Quiénes somos?</a></li>
+                        <li><a href="#services" className="hover:underline">Proyectos en curso</a></li>
+                        <li><a href="#about" className="hover:underline">Proyectos terminados</a></li>
+                        <li><a href="#contact" className="hover:underline">Productos y resultados</a></li>
+                        <li><a href="#team" className="hover:underline">Nuestro equipo</a></li>
                         <li><a href="#contact" className="hover:underline">Contáctanos</a></li>
                     </ul>
                 </nav>
             </div>
         </header>
-    )
-}
+    );
+};
+
+export default Header;
--- src/components/home/HeroSectionComponent.jsx
+++ src/components/home/HeroSectionComponent.jsx
@@ -3,13 +3,14 @@ import {Button} from "@/components/ui/button";
 
 export default function HeroSectionComponent() {
     return (
-        <section id="home" className="relative h-[600px] flex items-center justify-center text-white">
-            <div
-                className="absolute inset-0 bg-cover bg-center z-0"
-                style={{backgroundImage: "url('/placeholder.svg?height=600&width=1200')"}}
-            />
+        <section id="home" className="relative h-[650px] flex items-center justify-center text-white">
+
+            <Image className="z-0 object-cover" src={'/image/home/fondo.svg'} alt={'Fondo'}
+                   layout={'fill'}/>
+
             <div className="absolute inset-0 bg-black opacity-30 z-10"/>
-            <Image src={'/image/fondo.png'} priority alt={'Fondo'} layout={'fill'}/>
+
+
             <div className="relative z-20 text-center">
                 <h2 className="text-5xl font-bold mb-4">Soluciones tecnológicas para el campo del mañana</h2>
                 <p className="text-xl mb-8">Por un mayor acceso a una alimentación sana, diversa y estable de la
